6006850bf59acf19de530b9633acd946e8211993
replaceits <sid@sidw.me>
2021-09-05 19:44:28 -0400

    Iteration 2

--- src/Resume.js
+++ src/Resume.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import {GoLocation} from 'react-icons/go';
 import {FaPhoneAlt, FaHtml5, FaLess, FaJira, FaFigma, FaGithub, FaTrello} from 'react-icons/fa';
-import {SiJavascript} from 'react-icons/si';
+import {SiJavascript, SiApachecordova} from 'react-icons/si';
 import {DiCss3, DiJqueryLogo} from 'react-icons/di';
 import {CgTemplate} from 'react-icons/cg';
 
@@ -15,7 +15,7 @@ class Resume extends React.Component {
         <div className="resume-wrapper">
 
           <div className='title'>
-            <div className='avatar-wrapper'><div className='avatar'></div></div>
+            <div className='avatar-wrapper'></div>
             <div className='contact'>
               <h1>Sidney Williams</h1>
               <h5>Full Stack Developer</h5>
@@ -35,8 +35,9 @@ class Resume extends React.Component {
                 <li><FaHtml5 />&nbsp;&nbsp;&nbsp;&nbsp;HTML5</li>
                 <li><DiCss3 />&nbsp;&nbsp;&nbsp;&nbsp;CSS3</li>
                 <li><DiJqueryLogo />&nbsp;&nbsp;&nbsp;&nbsp;JQuery</li>
-                <li><FaLess />&nbsp;&nbsp;&nbsp;&nbsp;Jade</li>
-                <li><CgTemplate />&nbsp;&nbsp;&nbsp;&nbsp;LESS</li>
+                <li><CgTemplate />&nbsp;&nbsp;&nbsp;&nbsp;Jade</li>
+                <li><FaLess />&nbsp;&nbsp;&nbsp;&nbsp;LESS</li>
+                <li><SiApachecordova />&nbsp;&nbsp;&nbsp;&nbsp;Cordova</li>
               </ul>
 
               <h4>Software</h4>
@@ -50,62 +51,62 @@ class Resume extends React.Component {
             </div>
 
             <div className='main-content'>
-              <h3>Work Experience</h3>
-
+              <br />
+              <br />
               <div className='work'>
                 <div className='work-who'>
                   <div className='work-avatar cllc'></div>
                   <div className='work-who-info'>
-                    <h4>Clean Lines Lawn Care</h4>
+                    <h5>Clean Lines Lawn Care</h5>
                     <i>https://www.cleanlineslawncare.com/</i>
                   </div>
                 </div>
-                <h5 className='work-title'>Freelance Web Developer</h5>
+                <h6 className='work-title'>Freelance Web Developer</h6>
                 <p className='work-length'>Aug 2021 – Sep 2021 | 2 mos</p>
+                <p className='work-desc'>Complete design of site and ground-up development in React.</p>
               </div>
 
               <div className='work'>
                 <div className='work-who'>
                   <div className='work-avatar osmosis'></div>
                   <div className='work-who-info'>
-                    <h4>Osmosis - Knowledge Diffusion</h4>
+                    <h5>Osmosis - Knowledge Diffusion</h5>
                     <i>https://www.osmosis.org/</i>
                   </div>
                 </div>
-
-                
-                
-                <h5 className='work-title'>Full Stack Developer</h5>
+                <h6 className='work-title'>Full Stack Developer</h6>
                 <p className='work-length'>Jan 2019 – Jun 2021 | 2 yrs 6 mos</p>
+                <p className='work-desc'>Development and maintence of backend APIs. Improvements to video processing and streaming technologies. Lead in vulnerability triaging and resolution. Mobile app development. Upgrades to payment processor.</p>
               
-                <h5 className='work-title'>Frontend Developer</h5>
+                <h6 className='work-title'>Frontend Developer</h6>
                 <p className='work-length'>Mar 2018 – Jan 2019 | 11 mos</p>
+                <p className='work-desc'>Implementation of new features. Maintenance of existing pages. Bug fixing. Ensuring mobile responsiveness.</p>
 
-                <h5 className='work-title'>Frontend Developer Intern</h5>
+                <h6 className='work-title'>Frontend Developer Intern</h6>
                 <p className='work-length'>Jan 2018 – Mar 2018 | 3 mos</p>
+                <p className='work-desc'>Developed web pages based upon Figma design files.</p>
               </div>
-
+{/* 
               <div className='work'>
                 <div className='work-who'>
                   <div className='work-avatar cllc'></div>
                   <div className='work-who-info'>
-                    <h4>Clean Lines Lawn Care</h4>
+                    <h5>Clean Lines Lawn Care</h5>
                     <i>https://www.cleanlineslawncare.com/</i>
                   </div>
                 </div>
-                <h5 className='work-title'>Freelance Web Developer</h5>
+                <h6 className='work-title'>Freelance Web Developer</h6>
                 <p className='work-length'>Mar 2017 – Apr 2017 | 2 mos</p>
-              </div>
-
-              <h3>Education</h3>
+              </div> */}
 
               <div className='work'>
                 <div className='work-who'>
                   <div className='work-avatar ecpi'></div>
-                  <h4>ECPI University</h4>
+                  <h5>ECPI University</h5>
                 </div>
-                <h5 className='work-title'>Computer Science</h5>
-                <p className='work-length'>2014 – 2018 | 4 yrs, some college</p>
+                <h6 className='work-title'>Computer Science</h6>
+                <p className='work-length'>2014 – 2016 | 2 yrs, some college</p>
+                <p className='work-desc'>Focus on database programming.</p>
               </div>
             </div>
           </div>
